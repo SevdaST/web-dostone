41b62203c85b39e1adc63dd5424f7d6add6445b6
Sevda SAHIN <sevmuk@yahoo.com>
2022-06-03 00:00:18 +0300

    js added

--- js/urunler.js
+++ js/urunler.js
@@ -4,14 +4,49 @@ function traverten()
         document.getElementById("urun1").style.display="block";
         document.getElementById("card1title").innerHTML = "Cross Cut Traverten";
         document.getElementById("card1img").src = "images/traverten/crosscut.jpg";
+
         document.getElementById("urun2").style.display="block";
         document.getElementById("card2title").innerHTML = "Vein Cut Traverten";
         document.getElementById("card2img").src = "images/traverten/veincut.jpg";
-        
-   
 
-        // const fs = require('fs')
-        // const length = fs.readdirSync('/images/traverten').length
+        document.getElementById("urun3").style.display="block";
+        document.getElementById("card3title").innerHTML = "Silver Traverten";
+        document.getElementById("card3img").src = "images/traverten/silvertraverten.jpg";
+      }
+
+      function emprador()
+      {
+        allvisiblefalse();
+        document.getElementById("urun1").style.display="block";
+        document.getElementById("card1title").innerHTML = "Dark Emprador";
+        document.getElementById("card1img").src = "images/emprador/darkemprador.jpg";
+
+        document.getElementById("urun2").style.display="block";
+        document.getElementById("card2title").innerHTML = "Dark Crystal Emprador";
+        document.getElementById("card2img").src = "images/emprador/darkcrystalemprador.jpg";
+
+        document.getElementById("urun3").style.display="block";
+        document.getElementById("card3title").innerHTML = "Light Emprador";
+        document.getElementById("card3img").src = "images/emprador/lightemprador.jpg";
+      }
+      function greymarble()
+      {
+        allvisiblefalse();
+        document.getElementById("urun1").style.display="block";
+        document.getElementById("card1title").innerHTML = "Pietra Grey";
+        document.getElementById("card1img").src = "images/greymarble/pietragrey.jpg";
+
+        document.getElementById("urun2").style.display="block";
+        document.getElementById("card2title").innerHTML = "Tundra Grey";
+        document.getElementById("card2img").src = "images/greymarble/tundragrey.jpg";
+
+      }
+      function muglabeyaz()
+      {
+        allvisiblefalse();
+        document.getElementById("urun1").style.display="block";
+        document.getElementById("card1title").innerHTML = "Muğla Beyazı";
+        document.getElementById("card1img").src = "images/mugla/muglabeyaz.jpg";
       }
       function onyx()
       {
@@ -73,16 +108,81 @@ function traverten()
         document.getElementById("card11img").src = "images/granit/verdeguatemala.jpg";
 
       }
+      function seramik()
+      {
+        allvisiblefalse();
+
+        document.getElementById("urun1").style.display="block";
+        document.getElementById("card1title").innerHTML = "Amazonite";
+        document.getElementById("card1img").src = "images/seramik/amazonite.jpg";
+  
+        document.getElementById("urun2").style.display="block";
+        document.getElementById("card2title").innerHTML = "Bianca Extra";
+        document.getElementById("card2img").src = "images/seramik/biancaextra.jpg";
+
+        document.getElementById("urun3").style.display="block";
+        document.getElementById("card3title").innerHTML = "Onicepersia";
+        document.getElementById("card3img").src = "images/seramik/onicepersia.jpg";
+       
+        document.getElementById("urun4").style.display="block";
+        document.getElementById("card4title").innerHTML = "Furrwhite";
+        document.getElementById("card4img").src = "images/seramik/furrwhite.jpg";
+
+        document.getElementById("urun5").style.display="block";
+        document.getElementById("card5title").innerHTML = "Pandawhite";
+        document.getElementById("card5img").src = "images/seramik/pandawhite.jpg";
+
+        document.getElementById("urun6").style.display="block";
+        document.getElementById("card6title").innerHTML = "Verdeissorie";
+        document.getElementById("card6img").src = "images/seramik/verdeissorie.jpg";
+      
+        document.getElementById("urun7").style.display="block";
+        document.getElementById("card7title").innerHTML = "Calacata Ultra";
+        document.getElementById("card7img").src = "images/seramik/calacataultra.jpg";
+
+        document.getElementById("urun8").style.display="block";
+        document.getElementById("card8title").innerHTML = "Marquinanero";
+        document.getElementById("card8img").src = "images/seramik/marquinanero.jpg";
+
+        document.getElementById("urun9").style.display="block";
+        document.getElementById("card9title").innerHTML = "Machia";
+        document.getElementById("card9img").src = "images/seramik/machia.jpg";
+
+        document.getElementById("urun10").style.display="block";
+        document.getElementById("card10title").innerHTML = "Arabescatogold";
+        document.getElementById("card10img").src = "images/seramik/arabescatogold.jpg";
+
+        document.getElementById("urun11").style.display="block";
+        document.getElementById("card11title").innerHTML = "breach";
+        document.getElementById("card11img").src = "images/seramik/breach.jpg";
+
+        document.getElementById("urun12").style.display="block";
+        document.getElementById("card12title").innerHTML = "Blackfusion";
+        document.getElementById("card12img").src = "images/seramik/blackfusion.jpg";
+
+        document.getElementById("urun13").style.display="block";
+        document.getElementById("card13title").innerHTML = "Delmare";
+        document.getElementById("card13img").src = "images/seramik/delmare.jpg";
+
+        document.getElementById("urun14").style.display="block";
+        document.getElementById("card14title").innerHTML = "Greyflurry";
+        document.getElementById("card14img").src = "images/seramik/greyflurry.jpg";
+
+        document.getElementById("urun15").style.display="block";
+        document.getElementById("card15title").innerHTML = "Lourentgold";
+        document.getElementById("card15img").src = "images/seramik/lourentgold.jpg";
+
+      }
 
       function allvisiblefalse()
       {
         var urun=[];
 
-        for (var i = 1; i <= 12; i++) {
+        for (var i = 1; i <= 15; i++) {
           urun[i]="urun"+(i);
         }
 
-        for (var i = 1; i <= 12; i++) {
+        for (var i = 1; i <= 15; i++) {
           document.getElementById(urun[i]).style.display = "none";
           ////document.getElementById(urun[i]).style="visibility:hidden";
         } 
