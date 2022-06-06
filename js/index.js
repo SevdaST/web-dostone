64492ebe0475f2a06d7f8745369aa3d728e8ec53
Sevda SAHIN <sevmuk@yahoo.com>
2022-06-06 19:21:32 +0300

    topbar and logo chanmged  - js added by Sevda

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 
 $(document).ready(function(){
-  $("#logo").mouseenter(function(){
+  $("#logo2").mouseenter(function(){
+        $(this).attr("src","images/hamburopen.jpg");
         $("#menu").slideDown();
       });
+  // $("#logo2").click(function(){
+        
+  //     });
   $("#menu").mouseleave(function(){
       $(this).slideUp();
+      $("#logo2").attr("src","images/hamburclose.jpg");
   });
  
 });
